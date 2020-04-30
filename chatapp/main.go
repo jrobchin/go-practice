51781b8cdc62b198e6a39d99cd2793f9f93e244e
Jason Chin <jason.rob.chin@gmail.com>
2020-04-29 22:47:39 -0400

    simple web page

---
+++ chatapp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<head>
+					<title>Welcome to JChat</title>
+				</head>
+				<body>
+					<h1>Let's chat</h1>
+				</body>
+			</html>
+		`))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
