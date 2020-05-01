6463ec9c1d6ebe207415ac90693443f9ddee86ae
Jason Chin <jason.rob.chin@gmail.com>
2020-05-01 18:28:47 -0400

    copy chatapp to chatappv2

---
+++ chatappv2/cmd/chat/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+/*
+	Create a template handler that compiles
+	templates and puts it in the HTTP response.
+*/
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	template *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	loadCompileTemplate := func() *template.Template {
+		return template.Must(template.ParseFiles(filepath.Join("cmd", "chat", "templates", t.filename)))
+	}
+
+	if os.Getenv("DEBUG") == "true" {
+		t.template = loadCompileTemplate()
+	} else {
+		t.once.Do(func() {
+			t.template = loadCompileTemplate()
+		})
+	}
+
+	t.template.Execute(w, r)
+}
+
+func main() {
+	// Parse addr flag
+	addr := flag.String("addr", ":8080", "The server addr.")
+	flag.Parse()
+
+	// Create and run room for chat
+	r := newRoom()
+	go r.run()
+
+	// Assign handlers to paths
+	http.Handle("/", &templateHandler{filename: "home.html"})
+	http.Handle("/room", r)
+
+	// Start listening
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ chatappv2/cmd/chat/room.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jrobchin/go-practice/chatapp/internal/trace"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+	tracer  trace.Tracer
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.tracer.Trace("New client joined")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("Client left")
+		case msg := <-r.forward:
+			r.tracer.Trace("Message recieved: ", string(msg))
+			for client := range r.clients {
+				client.send <- msg
+				r.tracer.Trace(" -- sent to client")
+			}
+		}
+	}
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+
+	if err != nil {
+		log.Fatal("room.ServeHTTP:", err)
+		return
+	}
+
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
