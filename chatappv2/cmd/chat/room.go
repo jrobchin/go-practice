097ca25f2ab6969896132d42027d3eaf812e1a38
Jason Chin <jason.rob.chin@gmail.com>
2020-05-01 21:03:37 -0400

    use JSON and messages instead of bytes

--- chatappv2/cmd/chat/room.go
+++ chatappv2/cmd/chat/room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/jrobchin/go-practice/chatappv2/internal/trace"
+	"github.com/stretchr/objx"
 
 	"github.com/gorilla/websocket"
 )
@@ -15,7 +16,7 @@ const (
 )
 
 type room struct {
-	forward chan []byte
+	forward chan *message
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
@@ -24,7 +25,7 @@ type room struct {
 
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -51,7 +52,7 @@ func (r *room) run() {
 			close(client.send)
 			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
-			r.tracer.Trace("Message recieved: ", string(msg))
+			r.tracer.Trace("Message recieved: ", msg.Message)
 			for client := range r.clients {
 				client.send <- msg
 				r.tracer.Trace(" -- sent to client")
@@ -62,18 +63,21 @@ func (r *room) run() {
 
 func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	socket, err := upgrader.Upgrade(w, req, nil)
-
 	if err != nil {
 		log.Fatal("room.ServeHTTP:", err)
 		return
 	}
-
-	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get auth cookie:", err)
+		return
 	}
-
+	client := &client{
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
+	}
 	r.join <- client
 	defer func() { r.leave <- client }()
 	go client.write()
